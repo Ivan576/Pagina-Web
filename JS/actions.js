ec162eab3df3c5462ba3adb25a91ecd4ec5b372e
Athziri <s17120201@alumnos.itsur.edu.mx>
2020-11-05 11:54:39 -0600

    Acción de acceder a twitter para athzi y Joaquinz

--- JS/actions.js
+++ JS/actions.js
@@ -1,3 +1,11 @@
 function clickImageVyl(){
     window.open("https://twitter.com/CcRVyl", "", "");
 }
+
+function clickImageAthzi(){
+    window.open("https://twitter.com/Athziri80732043", "", "");
+}
+
+function clickImageJuaquinz(){
+    window.open("https://twitter.com/JuaquinzOficial", "", "");
+}
