c3c5a96e2884eab7ec0bc0f55ec42e0424928f32
Jesus Leyva <chucho.leyva91@gmail.com>
2020-11-07 19:44:50 -0600

    FrontEnd terminado (Creo).

--- JS/actions.js
+++ JS/actions.js
@@ -9,3 +9,45 @@ function clickImageAthzi(){
 function clickImageJoaquinz(){
     window.open("https://twitter.com/JuaquinzOficial", "", "");
 }
+
+function clickTutotiales(){
+    window.open("../html/Tutoriales.html", "_self");
+}
+
+function clickNoticias(){
+    window.open("../html/Noticias.html", "_self");
+}
+
+function clickRecursos(){
+    window.open("../html/Recursos.html", "_self");
+}
+
+function clickPreguntas(){
+    window.open("../html/Preguntas.html", "_self");
+}
+
+function addElement () {
+    var corr = document.getElementById("correo").value;
+    var ques = document.getElementById("pregunta").value;
+    if (corr == "" || !(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(corr))) {
+        corr = "Anónimo"
+    }
+    if (ques == ""){
+        document.getElementById("pregunta").focus();
+        alert('¡Debe ingresar una pregunta!');
+    } else{
+        var correo = document.createElement("h3");
+        var pregunta = document.createElement("p");
+        var salto = document.createElement("br");
+        document.getElementById("correo").value = "";
+        document.getElementById("pregunta").value = "";
+        var newCorr = document.createTextNode(corr + " preguntó:");
+        var newPreg = document.createTextNode(ques);
+        correo.appendChild(newCorr);
+        pregunta.appendChild(newPreg);
+        var currentDiv = document.getElementById("articuloBlog"); 
+        currentDiv.appendChild(correo);
+        currentDiv.appendChild(pregunta);
+        currentDiv.appendChild(salto);
+    }
+}
