9ba0dd10b569921776147c6731f23eb46481cb64
Jesus Leyva <chucho.leyva91@gmail.com>
2020-11-21 10:19:15 -0600

    Commit

--- Php/models/preguntas.php
+++ Php/models/preguntas.php
@@ -1,34 +1,32 @@
 <?php
     class Preguntas {
-    private $Id;
-    private $Usuario;
-    private $Pregunta;
-   
-
-    public function setId($Id){
-        $this->Id = $Id;
-    }
-
-    public function getId(){
-        return $this->Id;
-    }
-
-    public function setUsuario($Usuario){
-        $this->Usuario = $Usuario;
-    }
-
-    public function getUsuario(){
-        return $this->Usuario;
-    }
-
-    public function setPregunta($Pregunta){
-        $this->Pregunta = $Pregunta;
-    }
+        private $Id;
+        private $Usuario;
+        private $Pregunta;
     
-    public function getPregunta(){
-        return $this->Pregunta;
-    }
 
-   
+        public function setId($Id){
+            $this->Id = $Id;
+        }
+
+        public function getId(){
+            return $this->Id;
+        }
+
+        public function setUsuario($Usuario){
+            $this->Usuario = $Usuario;
+        }
+
+        public function getUsuario(){
+            return $this->Usuario;
+        }
+
+        public function setPregunta($Pregunta){
+            $this->Pregunta = $Pregunta;
+        }
+        
+        public function getPregunta(){
+            return $this->Pregunta;
+        }
     }
 ?>
